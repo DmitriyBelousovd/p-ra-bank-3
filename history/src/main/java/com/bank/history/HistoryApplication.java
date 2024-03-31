d9437dc098e53ac53ee6bcf2e3d5b6737510627e
DmitriyBelousovd <94719275+DmitriyBelousovd@users.noreply.github.com>
2024-03-31 15:04:58 +0300

    bk-4 добавил зависимость с модулем common и componentScan

--- history/src/main/java/com/bank/history/HistoryApplication.java
+++ history/src/main/java/com/bank/history/HistoryApplication.java
@@ -3,9 +3,11 @@ package com.bank.history;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
 
 @EnableEurekaClient
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.bank.common", "com.bank.history"})
 public class HistoryApplication {
     public static void main(String[] args) {
         SpringApplication.run(HistoryApplication.class, args);
